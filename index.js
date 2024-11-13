58812db488304c76da927162c981b48e22e757e5
Shahil-Dev <alfee9006@gmail.com>
2024-11-13 18:51:27 +0600

    array part done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-alert("Shirt up")
+//array-index
+const number = [23,32,45,78,32]
+console.log(number[2]); //array index
+console.log(number.length);
+number.push(100) //(pushing number)
+number.pop() //(pooping number)
+const x = number.pop() //(restore number)
+console.log(number);
+console.log(x);
+
+const fruits = ["aam","jam","katal","lichu"]
+const flower = ["golap","shapla","joba","beli","krishnochura"]
+const array = fruits.concat(flower) //(added array)
+const find = fruits.indexOf("jam") //(to find index)
+console.log(flower.includes("shahil"));//(to find item)
+const join = flower.join("-")
+flower.unshift("kodom");
+
+console.log(flower);
+console.log(find);
+
+
